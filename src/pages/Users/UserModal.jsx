aaee82859da8d5fb8507d3550e6e96fec4e09f4d
IPmanKZ <89822893+IPmanKZ@users.noreply.github.com>
2021-12-05 20:59:44 +0600

    Add files via upload

--- src/pages/Users/UserModal.jsx
+++ src/pages/Users/UserModal.jsx
@@ -1,38 +1,130 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import makeStyles from "@mui/styles/makeStyles";
+import { DeleteOutline } from "@mui/icons-material";
 
-export default function FormDialog({open,setOpen}) {
+const useStyles = makeStyles((theme) => ({
+  dialogContent: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    minWidth: "400px",
+  },
+  dialogActions: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between !important",
+  },
+  icon: {
+    paddingBottom: "3px",
+  },
+}));
 
-  const handleClose = () => {setOpen(false)}
-
+export default function FormDialog({
+  dialogState,
+  setDialogState,
+  editedObj,
+  setEditedObj,
+  makeAction,
+}) {
+  const handleAction = (e) => {
+    const action = e.target.name ? e.target.name : "cancel";
+    setDialogState({ open: false, action });
+    makeAction(action);
+  };
+  const handleChange = (event) => {
+    let value = event.target.value;
+    let name = event.target.name;
+    setEditedObj((prevalue) => {
+      return {
+        ...prevalue,
+        [name]: value,
+      };
+    });
+  };
+  const classes = useStyles();
+  console.log("dialogState.action = ", dialogState.action);
   return (
     <div>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We
-            will send updates occasionally.
-          </DialogContentText>
+      <Dialog open={dialogState.open} name="cancel" onClose={handleAction}>
+        <DialogTitle>
+          {dialogState.action === "change" ? `Изменить` : `Добавить`}
+        </DialogTitle>
+        <DialogContent className={classes.dialogContent}>
           <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
+            required
+            id="login"
+            name="login"
+            label="Логин"
+            type="text"
             fullWidth
-            variant="standard"
+            margin="normal"
+            variant="outlined"
+            defaultValue={editedObj ? editedObj.login : ""}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="password"
+            name="password"
+            label="Пароль"
+            type="password"
+            fullWidth
+            margin="normal"
+            variant="outlined"
+            defaultValue={editedObj ? editedObj.password : ""}
+            onChange={handleChange}
+          />
+
+          <TextField
+            required
+            id="firstname"
+            name="firstname"
+            label="Имя"
+            type="text"
+            fullWidth
+            margin="normal"
+            variant="outlined"
+            defaultValue={editedObj ? editedObj.firstname : ""}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="lastname"
+            name="lastname"
+            label="Фамилия"
+            type="text"
+            fullWidth
+            margin="normal"
+            variant="outlined"
+            defaultValue={editedObj ? editedObj.lastname : ""}
+            onChange={handleChange}
           />
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
+        <DialogActions className={classes.dialogActions}>
+          <div>
+            {dialogState.action === "change" ? (
+              <Button onClick={handleAction} name="delete" color="error">
+                <DeleteOutline className={classes.icon} /> Удалить
+              </Button>
+            ) : null}
+          </div>
+          <div>
+            <Button onClick={handleAction} name="cancel">
+              Отмена
+            </Button>
+            <Button
+              onClick={handleAction}
+              name={dialogState.action === "change" ? `change` : `add`}
+            >
+              {dialogState.action === "change" ? `Изменить` : `Добавить`}
+            </Button>
+          </div>
         </DialogActions>
       </Dialog>
     </div>
--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,51 +1,93 @@
-import React, { useState } from 'react'
-import DataTable from '../../components/DataTable';
-import {Add, Edit} from '@mui/icons-material';
-import {users} from '../../db/db'
-import UserModal from './UserModal'
+import React, { useState } from "react";
+import DataTable from "../../components/DataTable";
+import { Add, Edit } from "@mui/icons-material";
+import { users } from "../../db/db";
+import UserModal from "./UserModal";
 
 export default function Users() {
-    const [data,setData] = useState(users)
-    const [open,setOpen] = useState(false);
+  const [data, setData] = useState(users);
+  const [dialogState, setDialogState] = useState({ open: false, action: null });
+  const [editedObj, setEditedObj] = useState();
+  let newArray;
 
-    // const handleDelete = (id) => {
-    //     setData(data.filter((item) => item.id !== id));
-    // }
+  const makeAction = (action) => {
+    switch (action) {
+      case "add":
+        editedObj.id = Math.round(Date.now() + Math.random())
+        newArray = data.concat(editedObj)
+        setData(newArray);
+        break;
+      case "change":
+        const elementsIndex = data.findIndex(
+          (item) => item.id === editedObj.id
+        );
+        newArray = [...data];
+        newArray[elementsIndex] = { ...newArray[elementsIndex], ...editedObj };
+        console.log(newArray);
+        setData(newArray);
+        break;
+      case "delete":
+        setData(data.filter((item) => item.id !== editedObj.id));
+        break;
+      default: break;
+        // console.log("No case detected for action - ", action);
+    }
+  };
 
-    const handleClickOpen = () => {
-      setOpen(true);
-    };
+  const handleClickOpen = (params) => {
+    setEditedObj(params.row);
+    setDialogState({ open: true , action: params.row ? `change`:`add`});
+  };
 
-    const columns = [
-      { field: 'action', 
-        headerName:<Add style={{marginLeft:"6px",
-                                marginBottom:"0px !important",
-                                paddingTop:"25px",
-                                cursor : "pointer"}}
-                        onClick={handleClickOpen}
-                   />, 
-        width: 70,
-        sortable : false,
-        renderCell : (params) => {
-            return (
-                <>
-                 <Edit style={{marginLeft:"10px",
-                               cursor:"pointer"}} onClick={handleClickOpen}/>
-                </>
-            )
-        }
+  const columns = [
+    {
+      field: "action",
+      headerName: (
+        <Add
+          style={{
+            marginLeft: "6px",
+            marginBottom: "0px !important",
+            paddingTop: "25px",
+            cursor: "pointer",
+          }}
+          onClick={handleClickOpen}
+        />
+      ),
+      width: 70,
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Edit
+              style={{ marginLeft: "10px", cursor: "pointer" }}
+              onClick={() => handleClickOpen(params)}
+            />
+          </>
+        );
       },
-      { field: 'id', headerName: 'ID', width: 70 , hide : true},
-      { field: 'login', headerName: 'Логин', flex:1},
-      { field: 'firstname', headerName: 'Имя', flex:1},
-      { field: 'lastname', headerName: 'Фамилия',flex:1},
-      { field: 'password', headerName: 'Пароль',type: 'password',flex:1},
-    ];
+    },
+    { field: "id", headerName: "ID", width: 70, hide: true },
+    { field: "login", headerName: "Логин", flex: 1 },
+    { field: "firstname", headerName: "Имя", flex: 1 },
+    { field: "lastname", headerName: "Фамилия", flex: 1 },
+    { field: "password", headerName: "Пароль", type: "password", flex: 1 },
+  ];
 
-    return (
-            <>
-              <DataTable header="Пользователи" columns={columns} data={data} setData={setData}/>    
-              <UserModal open={open} setOpen={setOpen}/>
-            </>
-           )
+  return (
+    <>
+      <DataTable
+        header="Пользователи"
+        columns={columns}
+        data={data}
+        setData={setData}
+      />
+      <UserModal
+        dialogState={dialogState}
+        setDialogState={setDialogState}
+        editedObj={editedObj}
+        setEditedObj={setEditedObj}
+        makeAction={makeAction}
+      />
+    </>
+  );
 }
